b1b0def1167261c82abfc7eb2cb5c46d62b54596
joshuasemock <joshua.semock.pharmd@gmail.com>
2026-09-15 17:12:30 -0400

    Refactor App component to include ViewProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,37 @@ import { Routes, Route } from 'react-router-dom'
 import Nav from './Nav'
 import SearchView from './SearchView'
 import DrugDetail from './DrugDetail'
+import { ViewProvider } from './views'
+import Home from './pages/Home'
 import About from './pages/About'
 import Tools from './pages/Tools'
 import Resources from './pages/Resources'
 import Citations from './pages/Citations'
 import Blog from './pages/Blog'
 import BlogPost from './pages/BlogPost'
+import Account from './pages/Account'
 
 export default function App() {
   return (
-    <div className="min-h-full page-background">
-      <div className="page-content">
-        <Nav />
-        <Routes>
-          <Route path="/" element={<SearchView />} />
-          <Route path="/drugs/:slug" element={<DrugDetail />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/tools" element={<Tools />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/citations" element={<Citations />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<BlogPost />} />
-          <Route path="*" element={<SearchView />} />
-        </Routes>
+    <ViewProvider>
+      <div className="min-h-full page-background">
+        <div className="page-content">
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/browse" element={<SearchView />} />
+            <Route path="/drugs/:slug" element={<DrugDetail />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/tools" element={<Tools />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/citations" element={<Citations />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:slug" element={<BlogPost />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </ViewProvider>
   )
 }
